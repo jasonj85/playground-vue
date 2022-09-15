684e6d67f36f1b0f75d18446bb1481097e9480c8
Jason James <jason.james@cba-japan.com>
2022-09-15 10:00:18 +0100

    minor updates to joblistings

--- developer-jobs/src/components/JobListings/JobListings.vue
+++ developer-jobs/src/components/JobListings/JobListings.vue
@@ -2,7 +2,7 @@
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
       <JobListing
-        v-for="job in jobs"
+        v-for="job in currentPageJobs"
         :key="job.id"
         :job="job"
         data-test="job-listing"
@@ -22,6 +22,20 @@ export default {
     return {
       jobs: [],
     };
+  },
+  computed: {
+    currentPageJobs() {
+      const page = this.$route.query.page || 1;
+      const pageNumber = Number.parseInt(page);
+      if (pageNumber) {
+        const jobsPerPage = 10;
+        const firstJob = (pageNumber - 1) * jobsPerPage;
+        const lastJob = firstJob + jobsPerPage;
+        return this.jobs.slice(firstJob, lastJob);
+      }
+
+      return this.jobs.slice(0, this.jobsPerPage);
+    },
   },
   async mounted() {
     const response = await axios.get("http://localhost:3000/jobs");
--- developer-jobs/tests/unit/components/JobListings/JobListings.test.js
+++ developer-jobs/tests/unit/components/JobListings/JobListings.test.js
@@ -5,19 +5,38 @@ jest.mock("axios");
 import JobListings from "@/components/JobListings/JobListings.vue";
 
 describe("JobListings", () => {
+  const createRoute = (params = {}) => ({
+    query: {
+      page: "5",
+      ...params,
+    },
+  });
+
+  const createConfig = ($route) => ({
+    global: {
+      mocks: {
+        $route,
+      },
+    },
+  });
+
   it("fetches the jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
-    shallowMount(JobListings);
+    const $route = createRoute();
+
+    shallowMount(JobListings, createConfig($route));
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
-  it("creates the job listings for the correct number of jobs returned", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-    const wrapper = shallowMount(JobListings);
+  it("creates the job listings for a max of 10 jobs returned", async () => {
+    axios.get.mockResolvedValue({ data: Array(10).fill({}) });
+    const $route = createRoute({ page: "1" });
+
+    const wrapper = shallowMount(JobListings, createConfig($route));
 
     await flushPromises(); // wait for the promise to resolve
 
     const jobListings = wrapper.findAll("[data-test=job-listing]");
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
   });
 });
